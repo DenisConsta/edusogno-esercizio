9bd55d6fd7112fa2d1194b4d7655fc6828749299
DenisConsta <denisconstantin338@gmail.com>
2023-03-21 15:45:59 +0100

    first commit + styles

---
+++ views/partials/background.php
@@ -0,0 +1,13 @@
+
+<link rel="stylesheet" href="assets/styles/bg_style.css">
+
+<div class="bg-wrapper">
+  <img src="assets/images/vector_1.svg" alt="vector-1" class="vector-1 absolute">
+  <img src="assets/images/vector_4.svg" alt="vector-4" class="vector-4 absolute">
+  <img src="assets/images/vector_5.svg" alt="vector-5" class="vector-5 absolute">
+  <img src="assets/images/spacecraft.svg" alt="spacecraft" class="spacecraft absolute">
+  <img src="assets/images/ellipse_12.svg" alt="ellipse-12" class="ellipse-12 absolute">
+  <img src="assets/images/ellipse_13.svg" alt="ellipse-13" class="ellipse-13 absolute">
+
+
+</div>
---
+++ views/partials/header.php
@@ -0,0 +1,9 @@
+<!-- Header -->
+<header>
+  <!-- Navbar -->
+  <nav class="relative">
+    <div class="logo">
+      <img src="assets/images/logo.svg" alt="EduSogno">
+    </div>
+  </nav>
+</header>
